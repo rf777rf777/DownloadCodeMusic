0e6da3a303c0af9b0c9f11893c6fa28e7e30acfc
rf777rf777 <rf777rf777@gmail.com>
2018-03-09 22:19:40 +0800

    make detail

--- main.py
+++ main.py
@@ -2,6 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import time
+import os
+
+def chooseSong():
+	allSong = getUrl()
+	print('\n======================== Song List ========================\n\nDownload songs from "http://musicforprogramming.net/"\n\n===========================================================\n')
+	for name in allSong.keys():
+		index = list(allSong.keys()).index(name)
+		#print(list(allSongName).index(i) % 5)
+		if (index+1) % 3 == 0 :
+			print(name)
+		else:
+			print('{0: <30}'.format(name), end=' ')
+	
+	songLink = []
+	while True:
+		choice = input("\n\nPlease enter song numbers ( ex : 1 , 10 , 25)：")
+		try:
+			choice = choice.split(',')
+			for i in choice:
+				songLink.append(list(allSong.items())[int(i)-1])
+			break
+		except Exception as e:
+			print("\nError,Please try again. ( ex : 1 , 10 , 25)")
+			pass
+
+	return songLink
 
 def getUrl():
 	head = {
@@ -17,17 +43,15 @@ def getUrl():
 		if 	musicName.startswith("0"):
 			musicName = musicName[1:]
 		musicLinks[musicName] = '{0}{1}.mp3'.format(urlFront,musicName)
-	new = {}
-	new.update(list(musicLinks.items())[0:5])
-	
-	return new
+
+	return musicLinks
 
 def downloadMP3(link):
+	folder = folderDefine()
 	fileName , url = link
 	r = requests.get(url, stream=True)
-	with open('{0}.mp3'.format(fileName), 'wb') as f:
-
-		for chunk in r.iter_content(chunk_size=4096): 
+	with open('{0}/{1}.mp3'.format(folder, fileName), 'wb') as f:
+		for chunk in r.iter_content(chunk_size=512): 
 			if chunk:
 				f.write(chunk)
 		f.close()
@@ -45,11 +69,11 @@ def forloopDownload():
 import gevent
 from gevent import monkey
 def geventDownload():
-	urls = getUrl()
+	urls = chooseSong()
 	monkey.patch_all()
 
-	jobs = [gevent.spawn(downloadMP3, item) for item in urls.items()]
-
+	jobs = [gevent.spawn(downloadMP3, item) for item in urls]
+	print("Downloading...\n")
 	complete = 0
 	completeInTerminal(complete,len(urls))
 	for job in jobs:
@@ -59,11 +83,29 @@ def geventDownload():
 
 def completeInTerminal(complete,total):
 	done = int(30 * complete / total)
-	sys.stdout.write("\r已完成(%s/%s)[%s%s]" % (complete, total, '#'*done, '-'*(30 - done)))
+	sys.stdout.write("\rCompleted：( %s / %s )[ %s%s ]" % (complete, total, '#'*done, '-'*(30 - done)))
 	sys.stdout.flush()
+
+def folderDefine():
+	#if執行exe檔
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+        #print('系統名稱:'+os.name)
+        if os.name == 'nt':
+            symbol = "\\"
+        else :
+            symbol = "/"
+        musicFloder_path = application_path.rpartition(symbol)[0]+'/{0}'.format("CodingMusic")
+    #if直接執行.py檔
+    elif __file__:
+        musicFloder_path = "CodingMusic" 
+    return musicFloder_path
 
 timeStart = time.time()
 geventDownload()
 #forloopDownload()
 timeEnd = time.time()
-print("\r\n共費時 : %s 秒" % (timeEnd-timeStart))
+print("\r\nTotal Time : %s seconds" % (timeEnd-timeStart))
+
+if os.name == 'nt':
+	input("\n<<< Press Enter >>>")
